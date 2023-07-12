6b2172b795188c2726a35971062ecd77cede1eab
Dussa Dharma Teja <dharmatejadussa@gmail.com>
2023-07-12 16:33:03 +0530

    register

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Components/Header/Header'
+import Register from './Components/Landing/Register'
+import Footer from './Components/Footer/Footer'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Register/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
